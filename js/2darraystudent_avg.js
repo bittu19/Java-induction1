70433e40143f13e93be95d224ca503e65188d1d9
bittu19 <84221649+bittu19@users.noreply.github.com>
2021-05-17 14:49:49 +0530

    Create 2darraystudent_avg.js

---
+++ js/2darraystudent_avg.js
@@ -0,0 +1,14 @@
+var student_scores =[[78,84,69],
+                    [65,81,45],[66,96,45]];
+for(i=0;i<student_scores.length;i++)
+  {
+    var total=0
+    console.log('avg marks of student ' +(i+1))
+    for( j=0;j<student_scores[i].length;j++)
+      {
+        total+=student_scores[i][j]
+      }
+    var avg=total/student_scores[i].length
+    console.log(avg)
+    
+    }
