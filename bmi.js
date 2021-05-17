eb5428241317a0e4d9afb0e33db61052939c0722
bittu19 <84221649+bittu19@users.noreply.github.com>
2021-05-17 12:28:13 +0530

    Create bmi.js

---
+++ bmi.js
@@ -0,0 +1,17 @@
+var weight=60
+var height=1.72
+
+var bmi=(weight)/(height*height)
+
+if(bmi<25)
+  {
+    console.log("normal weight")
+  }
+else if(bmi>=25 && bmi<=30)
+  {
+    console.log("over weight")
+  }
+else
+  {
+    console.log("obesce")
+  }
