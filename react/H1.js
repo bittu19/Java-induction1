ef2acac3c1fee96fec5b3655ff2533dd679f0c57
bittu19 <84221649+bittu19@users.noreply.github.com>
2021-05-19 21:41:08 +0530

    Add files via upload

---
+++ react/H1.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import user from './user'
+const H1 = () => {
+        return( <React.Fragment><h1 > Hello react component </h1>
+            <user></user>
+            </React.Fragment> );
+            
+}
+export default H1;
