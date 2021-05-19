4c7ec564b0d53336828b64eab05826e5b35d28ae
bittu19 <84221649+bittu19@users.noreply.github.com>
2021-05-19 21:40:34 +0530

    Add files via upload

---
+++ react/user.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const user =(props)=>
+{
+   // const name='bittu'
+   // const profession='developer'
+     return( <div> <h1>(props.name)</h1>
+             <h3>(props.profession)</h3> </div>
+        );
+}
+export default user;
